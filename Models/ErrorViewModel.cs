bec8faed1c0f43c03629dd0ad2fb590f28e83187
MinhoMinh <vohoangminh20py@gmail.com>
2025-10-01 10:31:38 +0700

    database entity init
    database init

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AttendanceManagementPayrollSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
