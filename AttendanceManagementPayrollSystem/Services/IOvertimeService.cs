dff08d9491b64edc5f45bda41f8a6e2d374974ee
tlutlu1811 <tramykrb0905@gmail.com>
2025-10-28 13:40:08 +0700

    Luan push OT history

---
+++ AttendanceManagementPayrollSystem/Services/IOvertimeService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using AttendanceManagementPayrollSystem.DTO;
+using AttendanceManagementPayrollSystem.Models;
+
+namespace AttendanceManagementPayrollSystem.Services.Interfaces
+{
+    public interface IOvertimeService
+    {
+        IEnumerable<OvertimeRequestDTO> GetOvertimeHistoryByEmployee(int empId);
+        OvertimeRequest GetById(int id);
+        void CreateOvertimeRequest(OvertimeRequest request);
+        void ApproveOvertimeRequest(int reqId, int approverId);
+        void RejectOvertimeRequest(int reqId, int approverId);
+    }
+}
