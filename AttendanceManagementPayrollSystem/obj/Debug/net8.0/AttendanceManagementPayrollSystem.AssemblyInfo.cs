b5f610623ec1114b44f7db5471f7aa32f15df53a
nttluan181 <tramykrb0905@gmail.com>
2025-11-12 18:53:31 +0700

    Xem va gan Bonus

--- AttendanceManagementPayrollSystem/obj/Debug/net8.0/AttendanceManagementPayrollSystem.AssemblyInfo.cs
+++ AttendanceManagementPayrollSystem/obj/Debug/net8.0/AttendanceManagementPayrollSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AttendanceManagementPayrollSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75d87fbdf2dee786068c4cd6a467adc2d272558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+862670f590c41759cce6be8443f7fc3d76ca4e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AttendanceManagementPayrollSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AttendanceManagementPayrollSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
